60e7e104ccd320d7791153da126d511e05a9a118
fdhhhdjd <nguyentientai10@gmail.com>
2024-06-13 10:53:50 +0700

    fix: cors code go

--- internal/middlewares/corsMiddleware.go
+++ internal/middlewares/corsMiddleware.go
@@ -10,18 +10,34 @@ import (
 // It allows requests from different origins to access the resources of the server.
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		allowedOrigins := []string{
+			"http://localhost:5173",
+		}
+
+		origin := c.Request.Header.Get("Origin")
+
+		var isAllowedOrigin bool
+		for _, allowedOrigin := range allowedOrigins {
+			if allowedOrigin == origin {
+				isAllowedOrigin = true
+				break
+			}
+		}
+
+		if !isAllowedOrigin {
+			c.Next()
+			return
+		}
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, PUT, PATCH, DELETE, GET")
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusMethodNotAllowed)
-			return
-		}
-
-		if c.Request.Method == "PUT" {
-			c.AbortWithStatus(http.StatusMethodNotAllowed)
+			c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, PUT, PATCH, DELETE, GET")
+			c.Header("Access-Control-Max-Age", "86400")
+			c.AbortWithStatus(http.StatusOK)
 			return
 		}
 
